3073ec5167eaf05c5c2c0bac6863654ee43dd0d3
Jeremy Wiebe <jeremyw76@live.com>
2019-11-10 13:37:47 -0600

    Added vue to project
    - Initialized vue in the project, in a new folder called
    final_project_font.
    - https://www.youtube.com/watch?v=o_z5pol6vZE for intial setup.
    - Installed axios for communication with the backend.
    - Began initial work on session management/authorization, in
    final_project_front/src/backend/index.js.
    - The intent is to communicate authentication tokens with devise in the
    backend.

---
+++ final_project_front/src/components/Signin.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <h1 class="title">Hello there!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+  created () {
+    this.checkSignedIn()
+  },
+  updated () {
+    this.checkSignedIn()
+  },
+  methods: {
+    signin () {
+      this.$http.plain.post('/signin', { email: this.email, password: this.password })
+        .then(response => this.signinSuccessful(response))
+        .catch(error => this.signinFailed(error))
+    },
+    signinSuccessful (response) {
+      if (!response.data.csrf) {
+        this.signinFailed(response)
+        return
+      }
+
+      localStorage.csrf = response.data.csrf
+      localStorage.signedIn = true
+      this.error = ''
+      this.$router.replace('/records')
+    },
+    signinFailed (error) {
+      this.error = (error.response && error.response.data && error.response.data.error) || ''
+      delete localStorage.csrf
+      delete localStorage.signedIn
+    },
+    checkSignedIn () {
+      if (localStorage.signedIn) {
+        this.$router.replace('/records')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ final_project_front/src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueAxios from 'vue-axios'
+import { securedAxiosInstance, plainAxiosInstance } from './backend/index'
+import './../node_modules/bulma/css/bulma.css'
+
+// import './main.css'
+console.log('hey')
+
+Vue.config.productionTip = false
+
+Vue.use(VueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance
+})
+console.log('yo-e-yo')
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  securedAxiosInstance,
+  plainAxiosInstance,
+  components: { App },
+  template: '<App/>'
+})
