c249ace27a3e98a3633e6d0a40659b5e4665ba09
Jeremy Wiebe <jeremyw76@live.com>
2019-11-12 14:27:11 -0600

    Added force_ssl to config/application.rb
    - Quit bugging me!

---
+++ config/application.rb
@@ -0,0 +1,42 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module FinalProject
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    # config.load_defaults 6.0
+
+    # Configuration to allow API calls from the front end.
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins 'http://localhost:8080'
+
+    #     resource '*',
+    #       headers: :any,
+    #       credentials: %w(access-token expiry token-type uid client),
+    #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    #   end
+    # end
+
+    # Forces https.
+    config.force_ssl = true
+
+    # Whitelists the front-end server for cross-site requests.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+         origins 'http://localhost:8080'
+         resource '*', :headers => :any, :methods => [:get, :post, :options], credentials: true
+       end
+    end
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
