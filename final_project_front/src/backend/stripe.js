8da97b80c992fd1defce50194fa67ee2c153ee25
Jeremy Wiebe <jeremyw76@live.com>
2019-11-28 13:09:29 -0600

    Remove front end from project
    - Something broke hard.
    - Webpack was no longer unable to run, for unknown reasons.
    - Created an entire new project for the front-end to live in.
    - Removed all files pertaining to the front end from this project.

--- final_project_front/src/backend/stripe.js
+++
@@ -1,9 +0,0 @@
-export default {
-  instance: Stripe('pk_test_FTjdbSHNGRt3XRjIOZZYmpRJ00hwbail7N'),
-  sessionId: undefined,
-  redirectToCheckout (successCallback, errorCallback)  {
-    this.instance.redirectToCheckout({
-      sessionId: this.sessionId
-    }).then(successCallback).catch(errorCallback)
-  }
-}
