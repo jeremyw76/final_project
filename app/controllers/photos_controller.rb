b1d8a3f451ede9cb84050c6d1b5da80f0b371e9c
jeremyw76 <jeremyw76@live.com>
2019-12-02 09:47:20 -0600

    Fix pagination and add filters
    - Hoped to combine the pagination and filter functions by allowing
    params to be passed to the PhotosController index action, where they
    would be parsed and separated into separate logic modules, where
    composite queries could be built and a final, paginated and filtered
    data set could be returned.
    - Created classes in PhotosHelper.rb to implement Query Object pattern
    and assist in filtering and paginating result sets.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,47 +1,21 @@
 class PhotosController < ApplicationController
 
   def index
-    page = params.key?(:page) ? params[:page] - 1 : 0
-    per_page = params.key?(:per_page) ? params[:per_page] : 10
+    category = params.key?(:category) ? params[:category] : nil
+    ids = params.key?(:ids) ? params[:ids] : nil
+    page = params.key?(:page) ? params[:page].to_i : 1
+    per_page = params.key?(:per_page) ? params[:per_page].to_i : nil
 
-    photo_count = Photo.count
-    max_pages = photo_count / per_page
+    page = page < 1 ? 1 : page
 
-    page = max_pages if page > max_pages
-    page = 0 if page < 0
+    id_filtered_photos = PhotosHelper::IdFilter.new(Photo.all)
+    category_filtered_photos = PhotosHelper::CategoryFilter.new(id_filtered_photos.subset_for_ids(ids))
+    photoset = PhotosHelper::Pagination.new(category_filtered_photos.subset_for_category(category)).paginated(page, per_page)
 
-    offset = page * per_page
+    session[:page] = page
+    session[:per_page] = per_page
+    session[:pages] = photoset[:pages]
 
-    photo_data = Photo.all.offset(offset).limit(per_page).with_attached_image.map do | photo |
-      {
-        id: photo.id,
-        small_url: small_image_url(photo),
-        large_url: large_image_url(photo),
-        description: photo.description,
-        price: photo.display_price,
-        price_in_cents: photo.value
-      }
-    end
-
-    render json: { images: photo_data, page: 1, per_page: 10, pages: max_pages }
-  end
-
-  def show
-  end
-
-  def small_image_url(photo)
-    if photo.image.attached?
-      rails_representation_url(photo.image.variant(resize: "600x600^"))
-    else
-      nil
-    end
-  end
-
-  def large_image_url(photo)
-    if photo.image.attached?
-      rails_representation_url(photo.image.variant(resize:"1000x1000"))
-    else
-      nil
-    end
+    render json: photoset
   end
 end
--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,3 +1,71 @@
 module PhotosHelper
+  class Pagination
+    include Rails.application.routes.url_helpers
 
+    def initialize(relation = Photo.all)
+      @relation = relation
+    end
+
+    def paginated(page, per_page)
+      per_page = @relation.size if per_page == nil
+      offset = (page - 1) * per_page
+      max_pages = (@relation.size / per_page.to_f).ceil
+
+      photo_data = @relation.offset(offset).limit(per_page).with_attached_image.map do | photo |
+        puts "PHOTO #{photo.id}"
+        {
+          id: photo.id,
+          small_url: small_image_url(photo),
+          large_url: large_image_url(photo),
+          description: photo.description,
+          price: photo.display_price,
+          price_in_cents: photo.value
+        }
+      end
+
+      { images: photo_data, page: page, per_page: per_page, pages: max_pages }
+    end
+
+    def small_image_url(photo)
+      if photo.image.attached?
+        Rails.application.routes.url_helpers.rails_representation_url(photo.image.variant(resize: "600x600^").processed, host: 'http://localhost:3000')
+      else
+        nil
+      end
+    end
+
+    def large_image_url(photo)
+      if photo.image.attached?
+        Rails.application.routes.url_helpers.rails_representation_url(photo.image.variant(resize:"1000x1000").processed, host: 'http://localhost:3000')
+      else
+        nil
+      end
+    end
+  end
+
+  class Filter
+    def initialize(relation = Photo.all)
+      @relation = relation
+    end
+  end
+
+  class IdFilter < Filter
+    def subset_for_ids(ids = nil)
+      unless ids == nil then
+        @relation.where(id: ids)
+      else
+        @relation
+      end
+    end
+  end
+
+  class CategoryFilter < Filter
+    def subset_for_category(category = nil)
+      unless category == nil then
+        @relation.includes(:tag).where(tag: {name: category})
+      else
+        @relation
+      end
+    end
+  end
 end
